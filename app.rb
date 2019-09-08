38b2ae2662f0296e90247df060025335814b9b50
Sophie Gu <sophieqgu@gmail.com>
2019-09-08 20:00:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do 
+    enable :sessions
+    set :session_secret, 'secret'
+  end 
+  
+  get '/' do 
+    erb :index 
+  end 
+  
+  post '/checkout' do 
+    @session = session 
+    @session[:item]=params[:item]
+    erb :receipt
+  end 
+    
+  
 end
